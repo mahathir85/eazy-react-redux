3a30d39a6720247b3742c2d235ca3a1639dd5257
Mahathir <del_xmen@yahoo.com>
2020-06-09 22:48:51 +0800

    remove dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+//const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
